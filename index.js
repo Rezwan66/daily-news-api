0c5ab46ded74ae4841b3aae6e655aa51842f14fc
Rezwan66 <shaikhrezwan66@gmail.com>
2023-08-31 00:15:09 +0600

    integrated card details with the fetched news

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const handleCategory = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
 
     const data = await response.json();
-    console.log(data.data.news_category);
+    // console.log(data.data.news_category);
     const categories = data.data.news_category;
 
     const tabContainer = document.getElementById('tab-container');
 
-    categories.slice(0,3).forEach(category => {
+    categories.slice(0, 3).forEach(category => {
         const div = document.createElement('div');
         div.innerHTML = `
             <a onclick="handleLoadNews('${category.category_id}')" class="tab tab-lifted">${category.category_name}</a>
@@ -21,6 +21,53 @@ const handleLoadNews = async categoryId => {
     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
     const data = await res.json();
     console.log(data.data);
+    const newsByCategory = data.data;
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
+
+    newsByCategory?.forEach(news => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card bg-base-100 shadow-xl">
+                <figure>
+                    <img src="${news?.image_url}" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        ${news.title.slice(0,42)}
+                        <div class="badge badge-secondary p-5">${news?.rating?.badge}</div>
+                    </h2>
+                    <p>
+                        ${news.details.slice(0,62)} ...read more
+                    </p>
+                    <div class="card-footer flex justify-between mt-8">
+                        <div class="flex">
+                            <div>
+                                <div class="avatar online">
+                                    <div class="w-14 rounded-full">
+                                        <img
+                                            src="${news?.author?.img}" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h6>${news?.author?.name}</h6>
+                                <small>${news?.author?.published_date}</small>
+                            </div>
+                        </div>
+                        <div class="card-detaild-btn">
+                            <button
+                                class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+                                Details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(div);
+    })
 }
 
 
